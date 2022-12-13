88c14bccdd182eeee5ebde4637933b0a5f85e808
Kevin <kevin@moredesign.studio>
2022-12-13 16:42:10 +0100

    rename title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite(['resources/css/app.scss', 'resources/js/app.jsx'])
-    <title>Dopravná APP</title>
+    <title>Laravel APP</title>
     @inertiaHead
 </head>
 <body>
