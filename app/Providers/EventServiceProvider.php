9ca188fa6b196f81647a0485f12f14cd7bc5bf0f
Kevin <kevin@moredesign.studio>
2022-12-13 22:35:30 +0100

    custom menu and plugins

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -28,6 +29,112 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        /**
+         * Add event listener for adminlte menu
+         */
+        Event::listen(BuildingMenu::class, static function (BuildingMenu $event) {
+            // Update "label" option to Blog menu item
+            $event->menu->add(// Navbar items:
+                [
+                    'type' => 'navbar-search',
+                    'text' => 'search',
+                    'topnav_right' => true,
+                ],
+                [
+                    'type' => 'fullscreen-widget',
+                    'topnav_right' => true,
+                ],
+
+                // Sidebar items:
+                [
+                    'type' => 'sidebar-menu-search',
+                    'text' => 'search',
+                ],
+                [
+                    'key' => 'blog',
+                    'text' => 'Blog',
+                    'url' => 'admin/blog',
+                    'icon' => 'far fa-fw fa-file',
+                    'label' => 5,
+                    'label_color' => 'success',
+                ],
+                [
+                    'key' => 'pages',
+                    'text' => 'pages',
+                    'url' => 'admin/pages',
+                    'icon' => 'far fa-fw fa-file',
+                    'label' => 4,
+                    'label_color' => 'success',
+                ],
+                ['header' => 'account_settings'],
+                [
+                    'text' => 'profile',
+                    'url' => 'admin/settings',
+                    'icon' => 'fas fa-fw fa-user',
+                ],
+                [
+                    'text' => 'change_password',
+                    'url' => 'admin/settings',
+                    'icon' => 'fas fa-fw fa-lock',
+                ],
+                [
+                    'text' => 'multilevel',
+                    'icon' => 'fas fa-fw fa-share',
+                    'submenu' => [
+                        [
+                            'text' => 'level_one',
+                            'url' => '#',
+                        ],
+                        [
+                            'text' => 'level_one',
+                            'url' => '#',
+                            'submenu' => [
+                                [
+                                    'text' => 'level_two',
+                                    'url' => '#',
+                                ],
+                                [
+                                    'text' => 'level_two',
+                                    'url' => '#',
+                                    'submenu' => [
+                                        [
+                                            'text' => 'level_three',
+                                            'url' => '#',
+                                        ],
+                                        [
+                                            'text' => 'level_three',
+                                            'url' => '#',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                        [
+                            'text' => 'level_one',
+                            'url' => '#',
+                        ],
+                    ],
+                ],
+                ['header' => 'labels'],
+                [
+                    'text' => 'important',
+                    'icon_color' => 'red',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'warning',
+                    'icon_color' => 'yellow',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'information',
+                    'icon_color' => 'cyan',
+                    'url' => '#',
+                ]);
+
+
+        });
     }
 
     /**
